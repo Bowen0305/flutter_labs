025c96df4f0788934055a8dff789555b55785f58
huan1478 <huan1478@purdue.edu>
2023-02-07 08:30:56 -0500

    'lab2'

---
+++ lab2/lib/AcceleroScreen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:selectable_circle/selectable_circle.dart';
+
+class AcceleroScreen extends StatefulWidget {
+  @override
+  _AcceleroScreenState createState() => _AcceleroScreenState();
+}
+
+class _AcceleroScreenState extends State<AcceleroScreen> {
+  @override
+  Widget build(BuildContext context) {
+    List<bool> dots = [false, false, false, false, false, false, false];
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Accelero Screen"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[0],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[1],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[2],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[3],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[4],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[5],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[6],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lab2/lib/GyroScreen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:selectable_circle/selectable_circle.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+class GyroScreen extends StatefulWidget {
+  @override
+  _GyroScreenState createState() => _GyroScreenState();
+}
+
+class _GyroScreenState extends State<GyroScreen> {
+  @override
+  List<bool> dots = [false, false, false, true, false, false, false];
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gyro Screen"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[0],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[1],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[2],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[3],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[4],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[5],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+                SelectableCircle(
+                  width: 30.0,
+                  isSelected: dots[6],
+                  color: Colors.transparent,
+                  selectedBorderColor: Colors.blue,
+                  selectedColor: Colors.blue,
+                  selectMode: SelectMode.simple,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
