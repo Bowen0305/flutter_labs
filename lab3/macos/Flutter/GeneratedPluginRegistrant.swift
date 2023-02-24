5cc877378e13e14ad40c1ceec3a0ced060e79fbb
huan1478 <huan1478@purdue.edu>
2023-02-24 00:40:40 -0500

    add bluetooth

--- lab3/macos/Flutter/GeneratedPluginRegistrant.swift
+++ lab3/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_blue
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterBluePlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
